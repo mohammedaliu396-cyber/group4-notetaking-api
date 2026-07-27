91c6246c7dd360b12aa0228022179713cd4700d8
Y302467 <yakubhafsah302@gmail.com>
2026-07-27 09:03:13 -0100

    Configured package.json and development environment

--- subgroup-c/app.js
+++ subgroup-c/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const app = express();
 
+// Parse incoming JSON requests
 app.use(express.json());
 
-app.use(express.json());
+const PORT = 3000;
 
-const PORT =3000;
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-Ste
