243255940e30b57f1799edbfd127db922a9d8d74
Ibrahim Soliu Ayobami <ibroscope89@gmail.com>
2026-07-29 13:53:48 +0100

    Add note taking API to subgroup-b

--- subgroup-b/app.js
+++ subgroup-b/app.js
@@ -1,18 +1,33 @@
-require('dotenv').config();
+const express = require('express');
 
-const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-//Home route
-app.get("/", (req, res) => {
-    res.send("Welcome to the Note-Taking API!");
-});
 
-//Middleware
 app.use(express.json());
 
-//Start Server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+
+let notes = [
+  { id: 1, title: 'First Note', content: 'This is my first test note.' }
+];
+
+
+
+// 1. CREATE
+app.post('/api/notes', (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({ error: 'Title and content are required.' });
+  }
+
+  const newNote = {
+    id: notes.length ? notes[notes.length - 1].id + 1 : 1,
+    title,
+    content
+  };
+
+  notes.push(newNote);
+  res.status(201).json(newNote);
 });
+
